a464e9e82ecb3c67e3927f907a568f3a0c4f1a61
JamieS56 <james.sebire@btinternet.com>
2021-05-03 14:04:22 +0000

    number of dots on grid now changes with the difficulty buttons.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,210 @@
 document.addEventListener("DOMContentLoaded", function() {
-            console.log("hi")
-            function startGame() {
-            
+    console.log("hi")
 
-            }
+    function startGame() {
 
-            function startShrink() {
+    }
 
-            }
+    function startShrink() {
 
-            function stopShrink() {
+    }
 
-            }
+    function stopShrink() {
 
-            function addScore() {
+    }
 
-            }
+    function addScore() {
 
-            function changeDifficulty(difficulty){
-                console.log(difficulty);
+    }
 
+    // code to set difficulty of game
+    for (let i = 0; i < 3; i++) {
+        document.getElementsByClassName("difficulty-button")[i].addEventListener("click", function() {
+            let difficulty = this.value
+            console.log(difficulty);
+            if (difficulty === "easy") {
+                document.getElementById("game-box").innerHTML= 
+            `<row class = "dot-row">
+               <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+            </row>
+            <row class = "dot-row">
+               <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+            </row>
+            <row class = "dot-row">
+               <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+            </row>`;
 
-            }
-            for(let i = 0; i < 3; i++){
-                
-                document.getElementsByClassName("difficulty-button")[i].addEventListener("click", function(){
-                    let difficulty = this.value
-                    console.log(difficulty);
-                }) 
-            }
+            }else if(difficulty ==="medium"){
+                 document.getElementById("game-box").innerHTML = 
+            `<row class = "dot-row">
+                <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+                <col>
+               <div class = "dot"></div>
+               </col>
+            </row>
+            <row class = "dot-row">
+                <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+                <col>
+               <div class = "dot"></div>
+               </col>
+            </row>
+            <row class = "dot-row">
+                <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+                <col>
+               <div class = "dot"></div>
+               </col>
+            </row>
+            <row class = "dot-row">
+                <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+                <col>
+               <div class = "dot"></div>
+               </col>
+            </row>`;
+            }else if(difficulty ==="hard"){
+                 document.getElementById("game-box").innerHTML = 
+                 `<row class = "dot-row">
+                <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+                <col>
+               <div class = "dot"></div>
+               </col>
+                <col>
+               <div class = "dot"></div>
+               </col>
+            </row>
+            <row class = "dot-row">
+                <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+                <col>
+               <div class = "dot"></div>
+               </col>
+                <col>
+               <div class = "dot"></div>
+               </col>
+            </row>
+            <row class = "dot-row">
+                <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+                <col>
+               <div class = "dot"></div>
+               </col>
+                <col>
+               <div class = "dot"></div>
+               </col>
+            </row>
+            <row class = "dot-row">
+                <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+                <col>
+               <div class = "dot"></div>
+               </col>
+                <col>
+               <div class = "dot"></div>
+               </col>
+            </row>
+            <row class = "dot-row">
+                <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+               <col>
+               <div class = "dot"></div>
+               </col>
+                <col>
+               <div class = "dot"></div>
+               </col>
+                <col>
+               <div class = "dot"></div>
+               </col>
+            </row>`
+        }
+
         })
+    }
+})
