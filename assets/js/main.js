daa333f727c0ded1e5fdef593b6ba6fcd08d62a2
JamieS56 <james.sebire@btinternet.com>
2021-05-05 19:37:21 +0000

    dots are now shrinking but all at the same time.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,28 @@
 document.addEventListener("DOMContentLoaded", function(){
     console.log("hi DOM")
 
+    anime({
+        targets: '.sq',
+        translateX: 250
+        });
+
 
     // this will run after difficulty is selected and choose which dot starts to shrink
     function startGame(difficulty){
         console.log("game Started")
-        let easyDotArray = [$("#dot-1"), $("#dot-2"), $("#dot-3"), $("#dot-4"), $("#dot-5"), $("#dot-6"), $("#dot-7"), $("#dot-8"), $("#dot-9")];
+
+        let inShrink = false
+        let easyDotArray = ["#dot-1", "#dot-2", "#dot-3", "#dot-4", "#dot-5", "#dot-6", "#dot-7", "#dot-8", "#dot-9"];
         let mediumDotArray = [$("#dot-1"), $("#dot-2"), $("#dot-3"), $("#dot-4"), $("#dot-5"), $("#dot-6"), $("#dot-7"), $("#dot-8"), $("#dot-9"), $("#dot-10"), $("#dot-11"), $("#dot-12"), $("#dot-13"), $("#dot-14"), $("#dot-15"), $("#dot-16")];
         let hardDotArray = [$("#dot-1"), $("#dot-2"), $("#dot-3"), $("#dot-4"), $("#dot-5"), $("#dot-6"), $("#dot-7"), $("#dot-8"), $("#dot-9"), $("#dot-10"), $("#dot-11"), $("#dot-12"), $("#dot-13"), $("#dot-14"), $("#dot-15"), $("#dot-16"), $("#dot-17"), $("#dot-18"), $("#dot-19"), $("#dot-20"), $("#dot-21"), $("#dot-22"), $("#dot-23"), $("#dot-24"), $("#dot-25")];
         
-        if (difficulty === "easy"){
+        if (difficulty === "easy" && inShrink === false){
              
             for(let i = 0; i<9; i++){           
                 let rNumber = Math.floor(Math.random() * easyDotArray.length)
                 console.log(rNumber)
-                //startShrink(easyDotArray[rNumber])
+                startShrink(easyDotArray[rNumber])
+                inShrink = true
                 easyDotArray.splice(rNumber, 1)  
                 console.log(easyDotArray) 
             }    
@@ -22,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function(){
             for(let i = 0; i<16; i++){           
                 let rNumber = Math.floor(Math.random() * mediumDotArray.length)
                 console.log(rNumber)
-                //startShrink(mediumDotArray[rNumber])
+                startShrink(mediumDotArray[rNumber])
                 mediumDotArray.splice(rNumber, 1)  
                 console.log(mediumDotArray) 
             }
@@ -31,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function(){
             for(let i = 0; i<25; i++){           
                 let rNumber = Math.floor(Math.random() * hardDotArray.length)
                 console.log(rNumber)
-                //startShrink(hardDotArray[rNumber])
+                startShrink(hardDotArray[rNumber])
                 hardDotArray.splice(rNumber, 1)  
                 console.log(hardDotArray)
             } 
@@ -40,8 +48,23 @@ document.addEventListener("DOMContentLoaded", function(){
       
 
     function startShrink(dot) {
+        console.log("shrink")
+        console.log(dot)
+        dot.css('background-Color', 'green')
+        anime({
+            targets: dot,
 
-    }
+            scale: {
+                value: 0,
+                duration: 6000,
+                delay: 100,
+                easing: 'linear'
+            },
+
+        })
+        inShrink = false
+
+    };
 
     function stopShrink() {
 
